cb0f9710ff45d68831060c0b2859b9d2e591f757
Ivchenko Artem <metrash.a70@gmail.com>
2022-11-17 17:55:35 +0300

    Updates

---
+++ precache-manifest.595ee66ca6e8b61771f3c1094d43eab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920df27cb4b2ec20bbb9c0222b57ab47",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "eeff05bccfc290536048",
+    "url": "/TodoList/static/css/main.bf8ca107.chunk.css"
+  },
+  {
+    "revision": "d4a184270d9ffe2ef56f",
+    "url": "/TodoList/static/js/2.374e65e5.chunk.js"
+  },
+  {
+    "revision": "56b5fd54b2df988dab4b2a63cdbc4a6f",
+    "url": "/TodoList/static/js/2.374e65e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeff05bccfc290536048",
+    "url": "/TodoList/static/js/main.737addb4.chunk.js"
+  },
+  {
+    "revision": "d35d623f929282414f0c",
+    "url": "/TodoList/static/js/runtime-main.0ff14eb0.js"
+  }
+]);
